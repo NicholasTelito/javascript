5e59793cc449f19f832828e1729a8dc77b4187c9
Nicholas Telito <nicholastelito@gmail.com>
2022-10-13 11:43:23 -0300

    ex015

---
+++ aula13/ex014.js
@@ -0,0 +1,5 @@
+var c = 1
+while (c <= 37) {
+    console.log (`edemar so conta até ${c}`)
+    c++
+}
---
+++ aula13/ex015.js
@@ -0,0 +1,5 @@
+var c = 1
+do {
+    console.log(`Aurora ja completou ${c} meses de idade`)
+    c++
+}while (c <= 10)
